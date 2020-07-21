a8f5c866cd09b7d3bbcf2851ea3b9991e0998679
Kallam Reddy <klsnreddy@gmail.com>
2020-07-20 20:00:12 -0700

    Removed cors

--- udagram-feed-api/src/server.ts
+++ udagram-feed-api/src/server.ts
@@ -1,4 +1,4 @@
-import cors from 'cors';
+// import cors from 'cors';
 import express from 'express';
 import { sequelize } from './sequelize';
 
@@ -7,6 +7,7 @@ import { IndexRouter } from './controllers/v0/index.router';
 import bodyParser from 'body-parser';
 
 import { V0MODELS } from './controllers/v0/model.index';
+import { config } from './config/config';
 
 (async () => {
   await sequelize.addModels(V0MODELS);
@@ -18,21 +19,21 @@ import { V0MODELS } from './controllers/v0/model.index';
   app.use(bodyParser.json());
 
   //CORS Should be restricted
-  // app.use(function(req, res, next) {
-  //   res.header("Access-Control-Allow-Origin", "http://localhost:8100");
-  //   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  //   next();
-  // });
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", config.url);
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+  });
 
-  app.use(cors({
-    allowedHeaders: [
-      'Origin', 'X-Requested-With',
-      'Content-Type', 'Accept',
-      'X-Access-Token', 'Authorization',
-    ],
-    methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
-    origin: config.url,
-  }));
+  // app.use(cors({
+  //   allowedHeaders: [
+  //     'Origin', 'X-Requested-With',
+  //     'Content-Type', 'Accept',
+  //     'X-Access-Token', 'Authorization',
+  //   ],
+  //   methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+  //   origin: config.url,
+  // }));
 
   app.use('/api/v0/', IndexRouter)
 
